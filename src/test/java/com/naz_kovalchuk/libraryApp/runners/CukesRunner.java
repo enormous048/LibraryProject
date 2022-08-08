b08a39ad8448f5e4031391a6addcfc30e152358a
NazKovalchuk <enormous048.gmail.com>
2022-08-08 14:02:45 +0300

    2 push

--- src/test/java/com/naz_kovalchuk/libraryApp/runners/CukesRunner.java
+++ src/test/java/com/naz_kovalchuk/libraryApp/runners/CukesRunner.java
@@ -16,7 +16,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/naz_kovalchuk/libraryApp/step_definitions",
         dryRun = false,
-        tags = "@wip"
+        tags = ""
 )
 public class CukesRunner {
 }
